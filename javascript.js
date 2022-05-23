535bc88b2dbf3c6b107f41c7d2fb4aadd14b55fd
kevinqtogitty <kevinq.to@gmail.com>
2022-05-23 18:10:17 +0200

    Added funtion to JS file to randomly choose rock papers or scissors

--- javascript.js
+++ javascript.js
@@ -1,5 +1,23 @@
-function computerPlay {
-    let computer = Math.floor(Math.random() * 100) 
+
+let computer = computerPlay();
+function computerPlay() {
+    let compChoice;
+    let randomNum = Math.floor(Math.random() * 100);
+    
+    if (randomNum <= 33)
+    {
+        compChoice = 'scissors';
+    }
+    else if (randomNum <= 66 && randomNum > 33)
+    {
+        compChoice = 'rock';
+    }
+    else if (randomNum <= 100 && randomNum > 66)
+    {
+        compChoice = 'paper';
+    }
+    
+    return compChoice;
 }
 
-console.log(somputer)
+console.log(computer);
